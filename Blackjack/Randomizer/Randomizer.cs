55782e33b16d4706bf5fc5bcc8dd1c567b27c4bc
vsm <vsm.upskilling@gmail.com>
2023-06-16 18:56:14 +0200

    added randomizer class library

---
+++ Blackjack/Randomizer/Randomizer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace RandomizerLibrary
+{
+    public class Randomizer
+    {
+        private Random random;
+
+        public Randomizer()
+        {
+            random = new Random();
+        }
+
+        public int Next(int maxValue)
+        {
+            return random.Next(maxValue);
+        }
+
+        public int GetSecureRandom()
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] randomNumber = new byte[1000];
+                rng.GetBytes(randomNumber);
+                int value = BitConverter.ToInt32(randomNumber, 0);
+                return value % 52;
+            }
+        }
+
+        public int GetSecureRng(int maxValue)
+        {
+            return RandomNumberGenerator.GetInt32(maxValue);
+        }
+    }
+}
